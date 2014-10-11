62bd924e96813f7bc58058c74fdbc212b46fa4d8
stanley zheng <dev.stanzheng@gmail.com>
2014-10-10 20:40:29 -0400

    adding clock

--- app.jsx
+++ app.jsx
@@ -6,14 +6,48 @@
 var App = React.createClass({
   render: function () {
   return <div>
-         <h1> Hello! </h1>
-        </div>
+              <h1> Hello! </h1>
+              <Clock/>
+         </div>
   }
 });
 
 
+var Clock = React.createClass({
+  getInitialState: function () {
+  var self = this;
+  setInterval(function () {
+    self.setState({time:Date.now()});
+  },500);
+    return {
+      "time": Date.now()
+    }
+  },
+  render: function () {
+  return <div>
+          <h2> the time </h2>
+          <h2> {this.state.time.toLocaleString()} </h2>
+         </div>
+   }
+});
+
+/** Timer**/
+var Timer = React.createClass({
+  getInitialState: function () {
+    // body...
+  },
+  render: function () {
+    return <div>
+            <h1> hello</h1>
+           </div>
+    // body...
+  }
+})
+
+/** Render them **/
 React.renderComponent(
     <App/>,
     document.getElementById('app')
 );
+
 })();
